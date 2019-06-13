c5473b7a08fecb21cf8d6cf2fff7964bca8a6610
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-06-13 11:58:52 +0800

    c++homework
    homework

--- c++homework/main.cpp
+++ c++homework/main.cpp
@@ -1,15 +1,47 @@
-//
-//  main.cpp
-//  c++homework
-//
-//  Created by s20181102929 on 2019/6/13.
-//  Copyright © 2019 s20181102929. All rights reserved.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+template <typename T>
+void sort(T *a,int size){
+    for(int i=0;i<size;i++){
+        cin>>a[i];
+    }
+    T t=a[0];
+    for(int i=0;i<size-1;i++){
+        for(int j=0;j<size-1;j++){
+            if(a[j]>a[j+1]){
+                t=a[j];
+                a[j]=a[j+1];
+                a[j+1]=t;
+            }
+        }
+    }
+    
+}
+
+template <class T>
+void display(T* a, int size){
+    for(int i=0; i<size-1; i++) cout<<a[i]<<' ';
+    cout<<a[size-1]<<endl;
+}
+int main() {
+    const int SIZE=10;
+    int a[SIZE];
+    char b[SIZE];
+    double c[SIZE];
+    string d[SIZE];
+    int ty, size;
+    cin>>ty;
+    while(ty>0){
+        cin>>size;
+        switch(ty){
+            case 1:sort(a,size); display(a,size); break;
+            case 2:sort(b,size); display(b,size); break;
+            case 3:sort(c,size); display(c,size); break;
+            case 4:sort(d,size); display(d,size); break;
+        }
+        cin>>ty;
+    }
     return 0;
 }
